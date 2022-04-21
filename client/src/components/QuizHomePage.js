278fc13770264de14ef44d5afba7ac8e37702d60
Grant Royseth <groyseth@gmail.com>
2022-04-21 17:34:18 -0500

    cmmit

--- client/src/components/QuizHomePage.js
+++ client/src/components/QuizHomePage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import NavBar from './NavBar'
 import './QuizHome.css'
 export default function QuizHomePage() {
@@ -7,6 +8,9 @@ export default function QuizHomePage() {
       <NavBar />
 
       <div className='quizSelect'>quiz selection</div>
+      <button>
+        
+      </button>
       </>
     
   )
