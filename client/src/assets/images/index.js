4fd68d5ff956a5d7b52c0b7e921d28a1cbd73569
Grant Royseth <groyseth@gmail.com>
2022-04-27 21:44:34 -0500

    added questions

---
+++ client/src/assets/images/index.js
@@ -0,0 +1,11 @@
+import tryangle from './tryangle.jpg'
+import square from './square.jpg'
+import trapiziod from './trapiziod.jpg'
+import circle from './circle.jpg'
+const images = {
+tryangle,
+square,
+trapiziod,
+circle
+}
+export default images;
--- client/src/pages/Quiz1.js
+++ client/src/pages/Quiz1.js
@@ -1,60 +1,93 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { Link, Route } from 'react-router-dom'
+import images from '../assets/images/index';
+import "./quiz1.css"
+export default function App() {
 
-export default function Quiz1() {
-    const questions = [
-        {
-            questionText: 'Which one is a tryangle?',
-            questionOptions: [
-                { answerText: 'Square', isCorrect: false },
-                { answerText: 'Tryangle', isCorrect: true },
-                {answerText: 'Circle', isCorrect: false},
-            ],
-        },
-        {
-            questionText: 'Which one is the number 1',
-            questionOptions: [
-                {answerText: '3', isCorrect: false},
-                {answerText: '1', isCorrect: true},
-                {answerText: '4', isCorrect: false},
-            ],
-        },
-    ]
+	const questions = [
+		{
+			questionText: 'Which one is a triangle',
+			answerOptions: [
+				{ answerText: <img src={images.tryangle} className='image' alt='test' />, isCorrect: true },
+				{ answerText: <img src={images.square} className='image' alt='square'  />, isCorrect: false },
+				{ answerText: <img src={images.trapiziod} className='image' alt='trapiziod'  />, isCorrect: false },
+				{ answerText: <img src={images.circle} className='image' alt='circle' />, isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Who is CEO of Tesla?',
+			answerOptions: [
+				{ answerText: 'Jeff Bezos', isCorrect: false },
+				{ answerText: 'Elon Musk', isCorrect: true },
+				{ answerText: 'Bill Gates', isCorrect: false },
+				{ answerText: 'Tony Stark', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'The iPhone was created by which company?',
+			answerOptions: [
+				{ answerText: 'Apple', isCorrect: true },
+				{ answerText: 'Intel', isCorrect: false },
+				{ answerText: 'Amazon', isCorrect: false },
+				{ answerText: 'Microsoft', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'How many Harry Potter books are there?',
+			answerOptions: [
+				{ answerText: '1', isCorrect: false },
+				{ answerText: '4', isCorrect: false },
+				{ answerText: '6', isCorrect: false },
+				{ answerText: '7', isCorrect: true },
+			],
+		},
+	];
 
 	const [currentQuestion, setCurrentQuestion] = useState(0);
 	const [showScore, setShowScore] = useState(false);
 	const [score, setScore] = useState(0);
 
-	const handleAnswerOptionClick = (isCorrect) => {
+	const handleAnswerOptionClick = ( isCorrect) => {
+		
 		if (isCorrect) {
 			setScore(score + 1);
 		}
 
 		const nextQuestion = currentQuestion + 1;
 		if (nextQuestion < questions.length) {
+			// if(!isCorrect || isCorrect===false){
+			// 	const test = event.target.id
+			// 	console.log(test);
+			// 	var element = document.getElementById("test");
+  			// 	element.classList.toggle("style");
+			// }
 			setCurrentQuestion(nextQuestion);
 		} else {
 			setShowScore(true);
 		}
 	};
 	return (
-		<div >
+		<div className='app'>
 			{showScore ? (
-				<div >
+				<div className='score-section'>
 					You scored {score} out of {questions.length}
+					<button ><Link to='/quizDashboard'>Home</Link> </button>
+					<button onClick={()=> window.location.reload()}> retry</button>
 				</div>
 			) : (
 				<>
-					<div >
-						<div >
+					<div className='question-section'>
+						<div className='question-count'>
 							<span>Question {currentQuestion + 1}</span>/{questions.length}
 						</div>
-						<div >{questions[currentQuestion].questionText}</div>
+						<div className='question-text'>{questions[currentQuestion].questionText}</div>
 					</div>
-					<div >
-						{/* {questions[currentQuestion].answerOptions.map((answerOption) => (
+					<div className='answer-section'>
+						{questions[currentQuestion].answerOptions.map((answerOption) => (
 							<button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
-						))} */}
+						))}
 					</div>
+					
 				</>
 			)}
 		</div>
