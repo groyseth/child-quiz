3487a9df87aa9e20a235aa156143ed69e7c3192d
Grant Royseth <groyseth@gmail.com>
2022-05-02 20:26:04 -0500

    commit

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -34,9 +34,6 @@ export default function NavBar() {
                          Logout
                       </Link>
                   </li>
-                   
-                 
-                 
               </ul>
               
           </nav>
